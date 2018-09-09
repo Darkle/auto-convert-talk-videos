b7c7885518036c9b3a0e20a64004150fb120e97c
Coop <coop.coding@gmail.com>
2018-09-09 17:19:02 +1000

    Comments

--- app/appMain-compiled.js
+++ app/appMain-compiled.js
@@ -206,6 +206,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
+// https://docs.microsoft.com/en-us/windows/desktop/CIMWin32Prov/setpriority-method-in-class-win32-process
 var belowNormalProcessPriorityId = 16384;
 var ffmpegDefaultParams = ['-filter:v', 'setpts=PTS/' + _config.speed, '-filter:a', 'atempo=' + _config.speed, '-threads', '1'];
 
